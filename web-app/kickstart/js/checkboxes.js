793f6884e477592231d0988fc83fa8ff7ab7ea69
Mark Nelissen <mn@taktik.be>
2015-02-18 17:32:44 +0100

    Code cleanup

--- web-app/kickstart/js/checkboxes.js
+++ web-app/kickstart/js/checkboxes.js
@@ -1,10 +1,11 @@
 if (typeof jQuery !== 'undefined') {
 	$(document).ready(function() {
+		var $body = $('body');
 		/**
 		 * A checkbox using Bootstrap CSS's radio buttons
 		 */
-		$('body').on('click.checkbox','.radiocheckbox .btn.on:not(.readonly)',function() {
-			var $checkbox = $(this).parent().siblings("input[type=checkbox]")
+		$body.on('click.checkbox','.radiocheckbox .btn.on:not(.readonly)',function() {
+			var $checkbox = $(this).parent().siblings("input[type=checkbox]");
 			$checkbox.attr('checked', true);
             $checkbox.change();
 			$(this).addClass("btn-primary");
@@ -24,24 +25,24 @@ if (typeof jQuery !== 'undefined') {
 				});
 			}
 		})
-        $('body').on('click.checkbox','.radiocheckbox .btn.off:not(.readonly)',function() {
-			var $checkbox = $(this).parent().siblings("input[type=checkbox]")
+        $body.on('click.checkbox','.radiocheckbox .btn.off:not(.readonly)',function() {
+			var $checkbox = $(this).parent().siblings("input[type=checkbox]");
 			$checkbox.attr('checked', false);
             $checkbox.change();
-			$(this).addClass("btn-primary")
-			$(this).siblings().removeClass("btn-primary")
+			$(this).addClass("btn-primary");
+			$(this).siblings().removeClass("btn-primary");
 		})
 		
 		/**
 		 * A checkbox using Bootstrap CSS's buttons
 		 */
-        $('body').on('click.checkbox','.buttoncheckbox:not(.readonly)',function() {
-			var $text = $(this).text()
-			$(this).text($(this).attr('data-complete-text'))
-			$(this).attr('data-complete-text', $text)
-			$(this).toggleClass("btn-primary")
-			$(this).removeClass("active")
-			var $checkbox = $(this).siblings("input[type=checkbox]")
+        $body.on('click.checkbox','.buttoncheckbox:not(.readonly)',function() {
+			var $text = $(this).text();
+			$(this).text($(this).attr('data-complete-text'));
+			$(this).attr('data-complete-text', $text);
+			$(this).toggleClass("btn-primary");
+			$(this).removeClass("active");
+			var $checkbox = $(this).siblings("input[type=checkbox]");
 			$checkbox.attr('checked', !$checkbox.attr('checked'));
             $checkbox.change();
 		})
